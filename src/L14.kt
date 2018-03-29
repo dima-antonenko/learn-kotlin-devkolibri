caaa0b5e664dac5ca7b306a3e66b81c483bda993
Dmitry Antonenko <antonenkodima12@gmail.com>
2018-03-29 10:35:13 +0300

    complite 14 lesson

---
+++ src/L14.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+
+    var index: Int = 0
+
+    /*
+    while (index < 10) {
+        println("index: $index")
+        index ++
+    }
+    */
+
+    /*
+    do {
+        println("index: $index")
+        index ++
+    } while (index < 10)
+    */
+
+    var nums = 1..10
+
+    /*
+    for (value in nums) {
+        println("$value")
+    }
+    */
+    for (value in 10 downTo 1) {
+        println("$value")
+    }
+
+
+}
--- src/l13.kt
+++ src/l13.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
 
     var mouse = SuperMouse()
-    
+
 }
