04300c5554d78f2e689e9d232f2dc9e292c5022b
Dmitry Antonenko <antonenkodima12@gmail.com>
2018-03-30 13:20:37 +0300

    complite 17 lesson

---
+++ src/l17.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val e1 = Expr.Const(1.2)
+    val e2 = Expr.Const(2.2)
+
+    val sum = Expr.Sum(e1, e2)
+
+    println(eval(e1))
+}
+
+fun eval(expr: Expr) : Double = when(expr) {
+    is Expr.Const -> expr.number
+    is Expr.Sum -> eval(expr.e1) + eval(expr.e2)
+
+    Expr.NotANumber -> Double.NaN
+}
